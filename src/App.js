786ae422cf3bff10e0f24b399f3eab3158b54cc4
dipo <hacubydipo>
2022-12-08 00:02:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes,Link } from "react-router-dom";
 import Counter from "./Components/CustomHook/useCounter";
 import Reducer from "./Components/Reducer/useReducer";
 import Error from "./Components/Error";
@@ -10,6 +10,15 @@ function App() {
     <div className="App">
       <Errorboundary>
       <BrowserRouter>
+        <nav className="Hero">
+          <h1 className="logo">Altschool Africa</h1>
+         <li><Link to="/">Custom Hook</Link></li> 
+         <li><Link to="/Reducer">Use Reducer</Link></li>  
+         <li><Link to="/Error">Error Page</Link></li>  
+
+        </nav>
+
+
         <Routes>
           <Route path="/" element={<Counter initialCount={0} />} />
           <Route path="/Reducer" element={<Reducer initialCount={0} />} />
